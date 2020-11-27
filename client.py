2793f909c9b6a9603e895a19fb75bd5a4116abbb
Ben Totten <ttotten@pdx.edu>
2020-11-27 13:48:00 -0800

    IO file removal added

--- client.py
+++ client.py
@@ -5,9 +5,14 @@
 
 import sys
 import re
+import os
+from os import path
+import subprocess
+
 
 nick = 'Guest'  # User can change this in irc with /nick <NICK>
 key = ''
+client = 'client.io'  # Name of actual FIFO .io
 
 
 # Reads in key for server connection
@@ -17,10 +22,25 @@ def read_key():
     print(key)
 
 
+def rm_old():
+    print(f'Searching for {client}')
+    status = path.exists(client)    # Check existance of io
+    if status == False:             # If doesnt exist, return
+        print(f'{client} cleared')
+        return
+    else:                           # If file exists, delete it
+        print(f'Attempting {bashCommand}')
+        os.remove(client)
+        if path.exists(client) is True:
+            print(f'Error: Unable to delete {client}')
+        return
+
 def main():
-    read_key():
+    read_key()  # Read key for server
+    rm_old()    # Remove old client.io
+    
 
 
 # Launch main
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
