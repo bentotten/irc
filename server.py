407d84e3cafbffa1cd12085f3ed220f860dd2343
Ben Totten <ttotten@pdx.edu>
2020-11-28 21:01:42 -0800

    switched out stderr for print

--- server.py
+++ server.py
@@ -18,39 +18,39 @@ def main():
         # print("Received: " + line,)
     # fifo.close()
 
-    sys.stderr.write('If no cert found, run:  context.load_cert_chain(certfile="cert.pem", keyfile="cert.pem")')
+    print('If no cert found, run:  context.load_cert_chain(certfile="cert.pem", keyfile="cert.pem")')
     context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
     context.load_cert_chain(certfile="cert.pem", keyfile="cert.pem")
 
     # Create TCP/IP scoket
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0) as sock:
         # Bind to port
-        sys.stderr.write('starting up on %s port %s' % serverAddress)
+        print('starting up on %s port %s' % serverAddress)
         sock.bind(serverAddress)
 
         # Listen for incomming connections
         sock.listen(1)
         # with context.wrap_socket(sock, server_side=True) as ssock:
-            # sys.stderr.write('waiting for a connection')
+            # print('waiting for a connection')
             # connection, clientAddress = ssock.accept()
         while True:
-            sys.stderr.write('waiting for a connection')
+            print('waiting for a connection')
             connection, clientAddress = sock.accept()
 
             # On connection
             try:
-                sys.stderr.write(f'Connection from {clientAddress}')
+                print(f'Connection from {clientAddress}')
 
                 # Get data in chunks and retransmit it
                 while True:
                     data = connection.recv(16)
-                    sys.stderr.write('received "%s"' % data)
+                    print('received "%s"' % data)
                     # Send data to connected clients
                     if data:
-                        sys.stderr.write('sending data back to the client')
+                        print('sending data back to the client')
                         connection.sendall(data)
                     else:
-                        sys.stderr.write(f'No more data from {clientAddress}')
+                        print(f'No more data from {clientAddress}')
                         break
             finally:
                 # Close connection
